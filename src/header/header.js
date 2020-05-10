f2ad8a94a0ab9399ede2295648febce6f3973b20
shantanusohni <60313951+shantanusohni@users.noreply.github.com>
2020-05-10 20:28:40 +0530

    Update header.js

--- src/header/header.js
+++ src/header/header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <div class="header">
-            Simple React App_jaimatadi_jaimatadi
+            Simple React App_jaimatadi123
         </div>
     )
     
